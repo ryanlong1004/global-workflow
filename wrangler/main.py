95c67eeb0adce115a6597e7c0ca350e02255e79a
Ryan Long <ryanlong1004@gmail.com>
2023-06-21 22:13:07 +0000

    Initial commit

--- wrangler/main.py
+++ wrangler/main.py
@@ -5,6 +5,8 @@ from lua_api import unique_module_names, unique_module_permutations
 from file_operations import write_to_csv
 import csv
 import logging
+import yaml
+from config import Config
 
 
 def module_files_list(pattern):
@@ -24,12 +26,22 @@ def main():
         datefmt="%Y-%m-%d %H:%M:%S",
         filename="wrangler.log",
     )
+    
+    with open('./test.yaml', 'r') as _file:
+        result = yaml.safe_load(_file)
+        config = Config(result)
+        print(config)
+        for x in config.scripts:
+            print(x)
+        
+    
 
-    lines = []
-    for x in module_files_list("/home/rlong/apps/global-workflow/modulefiles/*lua"):
-        with open(x, "r") as _file:
-            for y in _file:
-                lines.append(y.strip().replace("\n", ""))
+    # lines = []
+    # for x in module_files_list("/home/rlong/apps/global-workflow/modulefiles/*lua"):
+    #     with open(x, "r") as _file:
+    #         for y in _file:
+    #             lines.append(y.strip().replace("\n", ""))
+    
 
 
 if __name__ == "__main__":
